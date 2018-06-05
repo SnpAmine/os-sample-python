b47c6baa31829f48bd0da484acc4877ba2ed73c5
SnpAmine <o.manauzic@gmail.com>
2018-06-05 09:57:45 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,23 @@
 
+from flask import Flask, request, render_template
+import os
+
+
+
+application = Flask(__name__)
+
+@application.route("/script", methods=['POST'])
+def script():
+    input_string = request.form['data']
+    print(input_string)
+
+    os.system('python SendCommandsFunc.py ' + input_string)
+
+    return "backend response"
+
+@application.route('/')
+def static_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    application.run()
